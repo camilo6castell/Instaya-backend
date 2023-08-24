27cbde20807c72b6c0eeae93919e2019d56bbd21
Camilo6Castell <kml107@hotmail.com>
2023-08-24 14:02:24 -0500

    docker

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,53 +1,50 @@
-const express = require('express')
-const router = express.Router()
-const dotenv = require('dotenv')
-var bcrypt = require('bcryptjs');
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv");
+var bcrypt = require("bcryptjs");
 
-dotenv.config()
+dotenv.config();
 
-const userSchema = require('../models/users')
+const userSchema = require("../models/users");
 
+// USERS LOGIN
 
-// USERS 
-
-router.post('/:id', async (req, res) => {
-    const user = await userSchema.findOne({cc: req.params.id})
-    console.log(req.body.cc)
-    if (user) {
-        if (bcrypt.compareSync(req.body.password, user.password)) {
-            res.json({
-                statusCode: 200,
-                session: req.body.cc
-            })
-        } else {
-            res.json({
-                statusCode: 404,
-                status: "Verifica tu información y vuelve a intentarlo"
-            })
-        }
+router.post("/:id", async (req, res) => {
+  const user = await userSchema.findOne({ cc: req.params.id });
+  if (user) {
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      res.status(200).json({
+        session: req.body.cc,
+      });
     } else {
-        res.json({
-            statusCode: 404,
-            status: 'El usuario no existe'
-        })
-    }    
-})
-
-router.post('/', async (req, res) => {
-    const { name, cc, password } = req.body
-    const userT = await userSchema.find({cc: cc}).exec()
-    if (userT.length == 0){
-        const newUser = new userSchema({name, cc, password})
-        await newUser.save()
-        res.json({
-            statusCode: 200,
-            status: 'usuario guardado ;)'
-        })
-    } else {
-    res.json({
-        status: 'usuario ya existe'
-    })  
+      res.status(404).json({
+        message: "Verifica tu información y vuelve a intentarlo",
+      });
     }
-})
+  } else {
+    res.status(404).json({
+      message: "El usuario no existe",
+    });
+  }
+});
 
-module.exports = router
+// USER SINGUP
+
+router.post("/", async (req, res) => {
+  const { name, cc, password } = req.body;
+  const userT = await userSchema.find({ cc: cc }).exec();
+  if (userT.length == 0) {
+    const newUser = new userSchema({ name, cc, password });
+    await newUser.save();
+    res.json({
+      statusCode: 200,
+      status: "usuario guardado ;)",
+    });
+  } else {
+    res.json({
+      status: "usuario ya existe",
+    });
+  }
+});
+
+module.exports = router;
